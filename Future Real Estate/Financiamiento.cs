d9533332b8c5a24f8fa73b8f6ceb5c450f39df96
netormrz41c <20210069@santacecilia.edu.sv>
2022-05-31 09:58:04 -0600

    forms financiamiento

---
+++ Future Real Estate/Financiamiento.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Future_Real_Estate
+{
+    public partial class Financiamiento : Form
+    {
+        public Financiamiento()
+        {
+            InitializeComponent();
+        }
+    }
+}
